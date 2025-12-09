357e552c66044ef1317ff78519ae3b56cffde036
Fikry Ramadhana <58467587+fikryrmdhna@users.noreply.github.com>
2025-12-09 15:05:01 +0700

    chore: add google site verification and noindex

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,6 +74,8 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Testing.' },
+        { name: 'google-site-verification', content: 'jvSHStKnbDMnzMz6z0TJiG9UOuMM4LYbNBc9zGYhApQ' },
+        { name: 'robots', content: 'noindex, nofollow' },
       ],
       script: [
         // Google Extended Access scripts
